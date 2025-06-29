4183f08e685c3697721d6757c74585af510030c9
Muhammad Maaz Shaikh <shaikhsiddiqui931@gmail.com>
2025-06-29 09:01:44 -0800

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {  Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
+import Home from './pages/home/Home'
 import List from './pages/list/List'
 import Hotel from './pages/hotel/Hotel'
 
