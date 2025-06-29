cdc78c9db35725dafe9eabc2fff40b6831799971
Muhammad Maaz Shaikh <shaikhsiddiqui931@gmail.com>
2025-06-29 07:31:36 -0800

    frontend done

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import './home.css'
+import Navbar from "../../components/Navbar/Navbar";
+import Header from "../../components/header/Header";
+import Featured from "../../components/featured/Featured";
+import PropertyList from "../../components/propertylist/PropertyList";
+import Featuredproperties from "../../components/featuredproperties/featuredproperties";
+import MailList from "../../components/MailList/MailList";
+import Footer from "../../components/footer/Footer";
+
+const Home=()=>{
+    return(
+        <div>
+            <Navbar/>
+            <Header/>
+            <div className="homeContainer">
+                <Featured/>
+                <h1 className="homeTitle">Browse by Property Type</h1>
+                <PropertyList/>
+                <h1 className="homeTitle">Homes guests love</h1>
+                <Featuredproperties/>
+                <MailList/>
+                <Footer/>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
